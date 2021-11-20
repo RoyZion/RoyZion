5ecf73b73beadf06ca6b19910a2ffbc09e5a21b1
RoyZion <royzion94@gmail.com>
2021-11-20 15:29:05 +0200

    homework5

---
+++ JSintro.js
@@ -0,0 +1,24 @@
+function myFunction(){
+    {
+        var x, y, z
+        x=7;
+        y=5;
+        z=x+y;
+        var end;
+        end=z**y;
+        var bool;
+        bool=(x==y);
+    }
+        console.log(end);
+        document.getElementById("demo2").innerHTML=end;
+        console.log(bool);
+
+        var user = {
+            firstName: 'eli', lastName: 'Cohen', email: 'elicohen@gmail.com', age:25
+        }
+        console.log(user)
+        console.log(user.email)
+}
+ // comments
+
+
